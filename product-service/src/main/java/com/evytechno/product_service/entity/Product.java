550dd107f2b7d15fd7c055a58abd5c9a720014c9
ankitkarodiya18 <ankitkarodiya8@gmail.com>
2025-09-19 16:41:26 +0530

    spring boot multi-module project setup with parent pom and child pom

---
+++ product-service/src/main/java/com/evytechno/product_service/entity/Product.java
@@ -0,0 +1,23 @@
+package com.evytechno.product_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_name")
+    private String productName;
+
+}
---
+++ user-service/src/main/java/com/evytechno/user_service/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.evytechno.user_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping
+    public String sayHello(){
+        return "Ram Ram";
+    }
+}
