b37eecbbe2cfc915c5d9f9c4e1b5705287f0e0e0
ankitkarodiya18 <ankitkarodiya8@gmail.com>
2025-09-30 17:59:53 +0530

    eureka implementation

--- product-service/src/main/java/com/evytechno/product_service/feign/BlogClient.java
+++ product-service/src/main/java/com/evytechno/product_service/feign/BlogClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "blog-service",url = "${blog.service.url}")
+@FeignClient(name = "blog-service")
 public interface BlogClient {
 
     @GetMapping("blog/{id}")
--- user-service/src/main/java/com/evytechno/user_service/feign/ProductClient.java
+++ user-service/src/main/java/com/evytechno/user_service/feign/ProductClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-service", url = "${product.service.url}")
+@FeignClient(name = "product-service")
 public interface ProductClient {
         @GetMapping("/product/{id}")
         ProductResponseDto getProduct(@PathVariable Long id);
